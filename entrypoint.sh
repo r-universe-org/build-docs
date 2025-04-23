ac8d1c3d0ebda59b8a8af2ab47945068b2ff8537
Jeroen Ooms <jeroenooms@gmail.com>
2025-04-23 17:53:22 +0200

    Save used R version

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -l
 set -e
 
+# Store used R version in use
+RVERSION=$(Rscript -e "cat(as.character(getRversion()))")
+echo "RVERSION=$RVERSION" >> $GITHUB_OUTPUT
+
 Rscript -e "rodocs::build_site('$1', '$2', '$3')"
 echo "Action complete!"
